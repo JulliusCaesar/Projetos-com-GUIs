dfe597941a10213766289c199aca343a6da01214
Julio César <julliusrock138@gmail.com>
2022-11-09 19:30:25 -0300

    Melhorando a tela GUI

--- [01] - Primeiro Projeto/main.py
+++ [01] - Primeiro Projeto/main.py
@@ -1,13 +1,22 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Filename')],
-    [sg.Input(), sg.FileBrowse()],
+    [sg.Text('Escolha seu arquivo')],
+    [sg.Input(key='-FILE-'), sg.FileBrowse("Selecionar", key='-BROWSE-')],
     [sg.Button("Ok"), sg.Button("Cancel")],
 ]
 
 window = sg.Window("Meu Primeiro Projeto", layout)
+
+while True:  
+    event, values = window.read()
    
-event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        print('A tela foi fechada')
+        break
+    
+    print("Evento: ", event)
+    print("-" * 25)
+    print("Valor do input:", values["-FILE-"]) # 0 => Chave
 
 window.close()
