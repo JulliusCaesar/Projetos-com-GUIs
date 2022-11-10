907420ab81b688e7f7628805639e172e92c20a92
Julio César <julliusrock138@gmail.com>
2022-11-10 19:22:42 -0300

    novas atualizações

--- [01] - Primeiro Projeto/main.py
+++ [01] - Primeiro Projeto/main.py
@@ -1,12 +1,32 @@
 import PySimpleGUI as sg
 
+# sg.theme_previewer() # DarjTeal6
+# print(sg.theme_list())
+sg.theme("DarkTeal6")
+
 layout = [
-    [sg.Text('Escolha seu arquivo')],
-    [sg.Input(key='-FILE-'), sg.FileBrowse("Selecionar", key='-BROWSE-')],
-    [sg.Button("Ok"), sg.Button("Cancel")],
+    [
+        sg.Text('Escolha seu arquivo')
+    ],
+    [
+        sg.Input(key='-FILE-'), 
+        sg.FileBrowse("Selecionar", key='-BROWSE-')
+    ],
+    [
+        sg.Button("Ok", button_color=("black on grey"), key='-BTNOK-'), 
+        sg.Button("Cancel", button_color=("black on grey"))
+    ],
 ]
 
+# button_color = ("cor do texto", "Cor de fundo")
+# button_color = ("blue", "white")
+# button_color = ("blue on white")
+# button_color = ("#00FF6C", "#FFFF00")
+
 window = sg.Window("Meu Primeiro Projeto", layout)
+
+def update_input():
+    window["-FILE-"].update("")
 
 while True:  
     event, values = window.read()
@@ -14,9 +34,12 @@ while True:
     if event == sg.WIN_CLOSED:
         print('A tela foi fechada')
         break
-    
+    elif event == "-BTNOK-":
+        # print("Valor do input:", values["-FILE-"]) # 0 => Chave
+        sg.popup(f'O arquivo escolhido foi: {values["-FILE-"]}', title= "Arquivo Escolhido")
+        update_input()
     print("Evento: ", event)
-    print("-" * 25)
-    print("Valor do input:", values["-FILE-"]) # 0 => Chave
+    
+    
 
 window.close()
