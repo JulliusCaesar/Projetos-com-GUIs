30bb75b3b4f1c7eb81d57a136c9693ab19960707
Julio César <julliusrock138@gmail.com>
2022-11-13 18:14:19 -0300

    mudando alguns codigos

--- [05] Projeto Base Final/view.py
+++ [05] Projeto Base Final/view.py
@@ -2,7 +2,7 @@
 import PySimpleGUI as sg
 
 # Cria a janela principal
-def create_main_window():    
+def create_main_window(title=None, theme="DarkTeal6"):    
     # Definindo o nosso tema
     sg.theme("DarkTeal6")
 
@@ -12,12 +12,16 @@ def create_main_window():
             sg.Text("Olá Mundo!")
         ]
     ]
-
-    # Definindo o Titulo da janela
-    title = "Meu Ptograma Base"
+    
+    # Definindo o titulo da janela
+    if title is None:
+        title = "Meu Tiítulo"
+    else:
+        title = title
 
     # Criar a janela e deixa ela finalizável
     window = sg.Window(title, layout, finalize=True)
     
     # Retorna a nossa janela
     return window
+
