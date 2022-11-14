1c9c532f8180f5730c953459a5cead578efabbbb
Julio César <julliusrock138@gmail.com>
2022-11-14 16:49:41 -0300

    Codificando o restante das telas

--- [07] - Recrutamento ACME/views/main.py
+++ [07] - Recrutamento ACME/views/main.py
@@ -4,11 +4,14 @@ import PySimpleGUI as sg
 from menu.menu import create_main_window
 from registration.first import create_first_window
 from registration.second import create_second_window
+from registration.third import create_third_window
+from registration.finish import create_finish_window
+from visualizer.visualizer import create_visualizer_window
 
 if __name__ == "__main__":
 
     # Definindo a janela inicial
-    window = create_second_window()
+    window = create_visualizer_window([["Julio", "10/05/1988", "5", "2021", "1000"]])
 
     # Loop da leitura da janela
     while True:
