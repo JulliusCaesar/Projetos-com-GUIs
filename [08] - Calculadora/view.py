32bd7147f96cacf24d173e18ff1109136d3da972
Julio César <julliusrock138@gmail.com>
2022-11-15 11:25:40 -0300

    Adicionando os botões e modificando o layout

--- [08] - Calculadora/view.py
+++ [08] - Calculadora/view.py
@@ -29,6 +29,38 @@ def create_main_window(title=None, theme="DarkTeal9"):
     
     image = resize_image(fullpath)
     
+    column_numbers = [
+        [
+            sg.Button('7', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('8', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('9', font=("Segoi UI", 20), size=(2, None)), 
+        ],
+        [
+            sg.Button('4', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('5', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('6', font=("Segoi UI", 20), size=(2, None)), 
+        ],
+        [
+            sg.Button('1', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('2', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('3', font=("Segoi UI", 20), size=(2, None)), 
+        ],
+        [
+            sg.Button('C', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('0', font=("Segoi UI", 20), size=(2, None)),
+            sg.Button('.', font=("Segoi UI", 20), size=(2, None)), 
+        ],
+    ]
+    # Nesta coluna se atentar a criar listas dentro de listas
+    column_operators = [
+        [sg.Button('<<', font=("Segoi UI", 15), size=(2, None))],
+        [sg.Button('/', font=("Segoi UI", 15), size=(2, None))],
+        [sg.Button('*', font=("Segoi UI", 15), size=(2, None))],
+        [sg.Button('-', font=("Segoi UI", 15), size=(2, None))],
+        [sg.Button('+', font=("Segoi UI", 15), size=(2, None))],
+        [sg.Button('=', font=("Segoi UI", 15), size=(2, None))],
+    ]
+    
     # Definindo nosso layout
     layout = [
         [
@@ -42,13 +74,21 @@ def create_main_window(title=None, theme="DarkTeal9"):
                 "0",
                 size=(10, 30),
                 font=("Segoi UI", 25),
+                justification='right',
+                readonly=True,
+                text_color='black',
+                key="-VALUE-"
             )
+        ],
+        [
+            sg.Column(column_numbers, background_color="darkgrey", pad=(0, 0)),
+            sg.Column(column_operators, background_color="grey", pad=(0, 5)),
         ]
     ]
     
     # Definindo o titulo da janela
     if title is None:
-        title = "Meu Tiítulo"
+        title = "Calculadora Antiga"
     else:
         title = title
 
