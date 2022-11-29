e02c1df85f3435fb60ee9b0a98c98730c179381e
Julio César <julliusrock138@gmail.com>
2022-11-29 19:27:15 -0300

    layout finalizado do projeto

--- [11] - Executador de Projetos/view.py
+++ [11] - Executador de Projetos/view.py
@@ -1,21 +1,81 @@
 # Importando o Pacote do PySimpleGUI
 import PySimpleGUI as sg
 
+def frame_search():
+    frame = [
+        [
+            sg.Text("Selecione a pasta com os projetos:"),
+        ],
+        [
+            sg.Input(key="-FOLDER-", size=(69, None)),
+            sg.FolderBrowse('Selecione a Pasta'),
+        ],
+        [
+            sg.Button('Buscar Arquivos', key="-SEARCH-"),
+        ],
+    ]
+    
+    return frame
+
+
+
+def frame_projects():
+    frame = [
+        [
+            sg.Text("Selecione um projeto:"),
+        ],
+        [
+            sg.Listbox([], size=(85, 11), key="-LIST-")
+        ],
+        [
+            sg.Button('Rodar Código', key="-RUN-"),
+        ],
+    ]
+    
+    return frame
+
+
+def frame_code():
+    frame = [
+        [
+            sg.Multiline("", size=(85, 20), key='-CODE-')
+        ]
+    ]
+    
+    return frame
+
+
+
 # Cria a janela principal
 def create_main_window(title=None, theme="DarkTeal6"):    
     # Definindo o nosso tema
     sg.theme(theme)
+    
+    column_left = [
+        [
+            sg.Frame('Buscar projetos', frame_search()),
+        ],
+        [
+            sg.Frame("Selecionar Projeto", frame_projects()),
+        ],
+    ]
+    
+    column_right = [
+        [
+            sg.Frame('Código', frame_code()),
+        ],
+    ]
 
     # Definindo nosso layout
     layout = [
         [
-            sg.Text("Olá Mundo!")
+            sg.Column(column_left), sg.Column(column_right)
         ]
     ]
     
     # Definindo o titulo da janela
     if title is None:
-        title = "Meu Tiítulo"
+        title = "Executador de Projetos"
     else:
         title = title
 
