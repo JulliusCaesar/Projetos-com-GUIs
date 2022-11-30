9881be7f3754810a245dbeee44dc6ac3c7679bfa
Julio César <julliusrock138@gmail.com>
2022-11-30 19:15:59 -0300

    aplicando a logica paea criar a lista

--- [11] - Executador de Projetos/main.py
+++ [11] - Executador de Projetos/main.py
@@ -1,3 +1,5 @@
+import glob
+
 # Importando o Pacote do PySimpleGUI
 import PySimpleGUI as sg
 
@@ -17,6 +19,18 @@ if __name__ == "__main__":
         if event == sg.WIN_CLOSED:
             break
         
+        elif event == "-SEARCH-":
+            path = values["-FOLDER-"]
+            full_path = glob.escape(path) + "/**/main.py"
+            all_projects = glob.glob(full_path, recursive=True)
+            
+            # all_projects = [project.replace(path, "")[1:] for project in all_projects]
+            
+            all_projects = [project.replace(path, "")[1:].replace("\main.py", '') for project in all_projects]
+            
+            window['-LIST-'].update(values=all_projects) # Recomendado usar values para o listbox
+            
+            
         # Mostrar o evento
         print(event, "==>", values) 
         
